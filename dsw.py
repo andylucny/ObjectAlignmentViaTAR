fix of histogram calculation for the IsoData threshold

dsw.py
@@ -45,7 +45,8 @@ def cyclicDSW(s, t, window=5, percentage=1, nms_radius=-1):
     for shift in range(n):
         costs[shift], paths[shift] = DSW(s, t, shift, window)
     
-    limit = np.max(costs)*percentage/100 + 1e-5
+    limit = np.min(costs)
+    limit += (np.max(costs)-limit)*percentage/100 + 1e-5
     #print('limit',limit)
     best_costs = []
     best_paths = []

retinex.py
@@ -16,7 +16,7 @@ def singleScaleRetinex(img, sigma, a=10):
     
 def isodataThreshold(src, weight=0.5):
     assert(src.dtype == np.uint8)
-    histogram, _ = np.histogram(src,256)
+    histogram, _ = np.histogram(src,bins=256,range=(0,255)) 
     count = np.cumsum(histogram)
     bins = np.arange(len(histogram)).astype(np.float)
     subtotal = np.cumsum(histogram*bins)